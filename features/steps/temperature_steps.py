047c097cb7a5f8438ef0d9a17e5e29da179afe9b
Anna Wester <annawester73@gmail.com>
2026-04-10 16:27:34 +0200

    commit with steps for convert  celsius to fahrenheit

--- features/steps/temperature_steps.py
+++ features/steps/temperature_steps.py
@@ -5,10 +5,23 @@ from behave import given, when, then
 def step_given_fahrenheit(context, value):
     context.value = value
 
+@given('temperaturen är {value:g} Celsius')
+def step_given_celsius(context, value):
+    context.value = value
+
+@when("temperaturen omvandlas till Fahrenheit")
+def step_when_to_fahrenheit(context):
+    context.result = context.value * 9 / 5 + 32
+
 @when("temperaturen omvandlas till Celsius")
 def step_when_to_celsius(context):
     context.result = (context.value - 32) * 5 / 9
 
 @then('ska resultatet vara {expected:g} Celsius')
 def step_then_celsius(context, expected):
+    assert round(context.result, 2) == round(expected, 2)
+
+
+@then('ska resultatet vara {expected:g} Fahrenheit')
+def step_then_fahrenheit(context, expected):
     assert round(context.result, 2) == round(expected, 2)
--- features/temperature_conversion.feature
+++ features/temperature_conversion.feature
@@ -10,3 +10,13 @@ Feature: Temperaturomvandling
     When temperaturen omvandlas till Celsius
     Then ska resultatet vara 21.1111 Celsius
 
+  Scenario: Omvandla Celsius till Fahrenheit
+    Given temperaturen är 100 Celsius
+    When temperaturen omvandlas till Fahrenheit
+    Then ska resultatet vara 212 Fahrenheit
+
+  Scenario: Omvandla Celsius till Fahrenheit med decimalresultat
+    Given temperaturen är 37.3 Celsius
+    When temperaturen omvandlas till Fahrenheit
+    Then ska resultatet vara 99.14 Fahrenheit
+
